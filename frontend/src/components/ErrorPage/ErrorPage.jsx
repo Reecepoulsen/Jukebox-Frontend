841ba0d1f44e2629df259525a6ad81441885dd83
Reece Poulsen <dreecepoulsen@gmail.com>
2022-11-04 15:32:40 -0600

    added wipe of profileData in LS on error

--- frontend/src/components/ErrorPage/ErrorPage.jsx
+++ frontend/src/components/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import BasicButton from '../BasicButton/BasicButton'
-import './ErrorPage.scss'
+import React from "react";
+import BasicButton from "../BasicButton/BasicButton";
+import "./ErrorPage.scss";
 
 export default function ErrorPage(props) {
   return (
-    <div className='errorPage'>
-      <div className='container'>
+    <div className="errorPage">
+      <div className="container">
         <h1>Oops Something Went Wrong</h1>
         <p>
           Looks like we have encountered an error, thank you for your patience.
         </p>
-        <BasicButton type={"submit"} text={"Reload"} OnClickFunc={() => props.setLoginToken(null)} />
+        <BasicButton
+          type={"submit"}
+          text={"Reload"}
+          OnClickFunc={() => {
+            props.setLoginToken(null);
+            localStorage.setItem("profileData", null);
+          }}
+        />
       </div>
     </div>
-  )
+  );
 }
