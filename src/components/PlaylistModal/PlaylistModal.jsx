1f08d5acbbac573112fa1aeae5a222d8ecdf35a5
Reece Poulsen <dreecepoulsen@gmail.com>
2022-11-12 14:24:24 -0700

    Added playlist modal

---
+++ src/components/PlaylistModal/PlaylistModal.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { RiDiscLine } from "react-icons/ri";
+import Loading from "../Loading/Loading";
+import "./PlaylistModal.scss";
+
+const loadSongs = async (playlistData) => {
+  return await fetch(
+    `${process.env.REACT_APP_BACKEND_URL}/profile/songs/${playlistData.id}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('loginToken')}`,
+        "Content-type": "application/json",
+      },
+    }
+  ).then((res) => res.json());
+};
+
+export default function PlaylistModal({ playlistData }) {
+  console.log("Playlistdata on playlist modal", playlistData)
+  const [tracks, setTracks] = useState(null);
+
+  useEffect(() => {
+    setTracks(loadSongs(playlistData));
+  }, []);
+
+  if (playlistData === null || tracks === null) {
+    return <Loading />;
+  }
+  const songList = [];
+  let counter = 0;
+
+  tracks.map((song) => {
+    songList.push(
+      <li className="song" key={counter}>
+        <span className="song__title">
+          {song.name.length > 24
+            ? song.name.substring(0, 24) + "..."
+            : song.name}
+        </span>
+        <RiDiscLine className="song__icon" size="24" />
+      </li>
+    );
+    counter++;
+  });
+
+  return (
+    <div className="playlistModal">
+      <div className="header">
+        <h2 className="header__title">{playlistData.name}</h2>
+        <p className="header__songCount">Songs: {playlistData.tracks.total}</p>
+        <div className="header__image">
+          <img
+            src={playlistData.images[0] ? playlistData.images[0].url : ""}
+            alt=""
+          />
+        </div>
+      </div>
+      <ul className="songList">{songList}</ul>
+    </div>
+  );
+}
