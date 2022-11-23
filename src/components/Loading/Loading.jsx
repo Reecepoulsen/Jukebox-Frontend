ea5a1af28923c5b0c052331bd95f124353d742f0
Reece Poulsen <dreecepoulsen@gmail.com>
2022-11-23 15:36:01 -0700

    Removed loading text

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,21 +1,18 @@
-import './Loading.scss'
+import "./Loading.scss";
 
 export default function Loading() {
   return (
-    <div className='loading'>
-      <div className="text">
-        <h2>Loading</h2>
-        <div className="logo-container">
-          <div className="circle c1"></div>
-          <div className="circle c2"></div>
-          <div className="circle c3"></div>
-          <div className="circle c4"></div>
-          <div className="circle c5"></div>
-          <div className="circle c6"></div>
-          <div className="circle c7"></div>
-          <div className="circle c8"></div>
-        </div>
+    <div className="loading">
+      <div className="logo-container">
+        <div className="circle c1"></div>
+        <div className="circle c2"></div>
+        <div className="circle c3"></div>
+        <div className="circle c4"></div>
+        <div className="circle c5"></div>
+        <div className="circle c6"></div>
+        <div className="circle c7"></div>
+        <div className="circle c8"></div>
       </div>
     </div>
-  )
+  );
 }
