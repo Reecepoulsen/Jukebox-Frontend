2932531188df8f59370729caa5e136e41de00086
Reece Poulsen <dreecepoulsen@gmail.com>
2022-11-23 21:57:35 -0700

    Started add/remove songs

--- src/components/ScrollableSongListIem/ScrollableSongListIem.jsx
+++ src/components/ScrollableSongListIem/ScrollableSongListIem.jsx
@@ -1,21 +1,38 @@
+import { useState } from "react";
 import { RiDiscLine, RiDiscFill } from "react-icons/ri";
 import "./ScrollableSongListItem.scss";
 
-const ScrollableSongListItem = ({ song, songSpotlight ,setSongSpotlight }) => {
+const ScrollableSongListItem = ({ song, songSpotlight, setSongSpotlight }) => {
+  const [savedSong, setSavedSong] = useState(false);
+
+  let discIcon = null;
+  if (savedSong) {
+    discIcon = <RiDiscFill className="scrollableSongListItem-Icon" size="24" onClick={() => {
+      console.log("Remove Song")
+      setSavedSong(false);
+    }} />
+  } else {
+    discIcon = <RiDiscLine className="scrollableSongListItem-Icon" size="24" onClick={() => {
+      console.log("Add Song")
+      setSavedSong(true);
+    }}/>
+  }
 
   return (
     <li
-      className={`scrollableSongListItem ${songSpotlight.id === song.id ? "activeSong" : ""}`}
-      onClick={() => {
-        setSongSpotlight(song);
-      }}
+      className={`scrollableSongListItem ${
+        songSpotlight.id === song.id ? "activeSong" : ""
+      }`}
     >
-      <span className="scrollableSongListItem-Title">
-        {song.name.length > 14
-          ? song.name.substring(0, 14) + "..."
-          : song.name}
+      <span
+        className="scrollableSongListItem-Title"
+        onClick={() => {
+          setSongSpotlight(song);
+        }}
+      >
+        {song.name.length > 14 ? song.name.substring(0, 14) + "..." : song.name}
       </span>
-      <RiDiscLine className="scrollableSongListItem-Icon" size="24" />
+      {discIcon}
     </li>
   );
 };
